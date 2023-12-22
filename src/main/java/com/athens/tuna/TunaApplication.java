6bf84fdd4216c1dd73547ec8ef94e06861ee9e0c
n1chaudhary <n1chaudhary@bravurasolutions.com>
2023-12-22 13:31:31 +0530

    Initial Commit

---
+++ src/main/java/com/athens/tuna/TunaApplication.java
@@ -0,0 +1,13 @@
+package com.athens.tuna;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TunaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TunaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/athens/tuna/TunaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.athens.tuna;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TunaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
